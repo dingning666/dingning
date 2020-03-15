8b246f38f2b4bc2f790957d9a590aef0ce3f559d
dingning666 <2627497610@qq.com>
2020-03-15 15:51:32 +0800

    rollcall

---
+++ src/main/java/com/rollcall/RollcallApplication.java
@@ -0,0 +1,13 @@
+package com.rollcall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollcallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollcallApplication.class, args);
+	}
+
+}
