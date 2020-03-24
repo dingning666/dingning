5565392744fb759a6fc25f6d083ef42315fddbaf
dingning666 <2627497610@aqq.com>
2020-03-24 10:33:31 +0800

    demo

--- src/main/java/com/DemoApplication.java
+++ src/main/java/com/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.rollcall;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RollcallApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RollcallApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
