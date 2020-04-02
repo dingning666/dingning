0c3ffb34cd87597cf3943b448e0730681f65dc6f
dingning666 <2627497610@aqq.com>
2020-04-02 14:30:57 +0800

    ElasticSearch

---
+++ src/main/java/com/elasticsearch/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.elasticsearch.dao;
+
+import com.elasticsearch.entity.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface UserRepository extends ElasticsearchRepository<User,String> {
+
+}
---
+++ src/main/java/com/elasticsearch/entity/User.java
@@ -0,0 +1,60 @@
+package com.elasticsearch.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+
+@Document(indexName = "user",type = "user")
+public class User {
+    /**
+     * 用户代码
+     */
+    @Id
+    private String userCode;
+    /**
+     * 用户名称
+     */
+    private String userName;
+    /**
+     * 用户登录名
+     */
+    private String userAcct;
+    /**
+     * 用户密码
+     */
+    private String userPwd;
+
+    public String getUserCode() {
+        return userCode;
+    }
+
+    public void setUserCode(String userCode) {
+        this.userCode = userCode;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserAcct() {
+        return userAcct;
+    }
+
+    public void setUserAcct(String userAcct) {
+        this.userAcct = userAcct;
+    }
+
+    public String getUserPwd() {
+        return userPwd;
+    }
+
+    public void setUserPwd(String userPwd) {
+        this.userPwd = userPwd;
+    }
+
+
+}
